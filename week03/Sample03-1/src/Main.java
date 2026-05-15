cdb603c2711b42c701699aa23894a899917af66e
sj435 <Read25@kbu.ac.kr>
2026-05-15 09:20:28 +0900

    week03

--- week03/Sample03-1/src/Main.java
+++ week03/Sample03-1/src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Book book = new Book("자바의 정석");
+        Book book = new Book("자바의  정석");
         Student student = new Student("김영희");
         Teacher teacher = new Teacher("김철수","자바");
 
